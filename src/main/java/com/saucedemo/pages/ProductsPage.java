5358f85526bc58f92aa80672b8f51b2c7b927de5
Waihon Yew <yewwaihon@gmail.com>
2025-05-21 20:12:28 +0800

    Create Products page which is transitioned from Login Page

---
+++ src/main/java/com/saucedemo/pages/ProductsPage.java
@@ -0,0 +1,13 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.By;
+
+public class ProductsPage extends BasePage {
+
+    private By productsHeader = By.xpath("//span[text()='Products']");
+
+    public boolean isProductsHeaderDisplayed() {
+        return find(productsHeader).isDisplayed();
+    }
+
+}
