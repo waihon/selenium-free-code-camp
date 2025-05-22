62144d9c65350456f0dfb9f899b850b085d0c460
Waihon Yew <yewwaihon@gmail.com>
2025-05-23 01:09:56 +0800

    Use CSS selector instead of XPath to to find the error message

--- src/main/java/com/saucedemo/pages/LoginPage.java
+++ src/main/java/com/saucedemo/pages/LoginPage.java
@@ -7,7 +7,7 @@ public class LoginPage extends BasePage {
     private By usernameField = By.id("user-name");
     private By passwordField = By.id("password");
     private By loginButton = By.id("login-button");
-    private By errorMessage = By.xpath("#login_button_container h3");
+    private By errorMessage = By.cssSelector("#login_button_container h3");
 
     public void setUsername(String username) {
         set(usernameField, username);
